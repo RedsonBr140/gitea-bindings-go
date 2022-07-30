ce2914db0977fc2acf225fd9e35f83a107a84e56
Redson <redson@riseup.net>
2022-07-29 22:34:53 -0300

    feat: followers and following

---
+++ gitea/users_followers.go
@@ -0,0 +1,45 @@
+package gitea
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+// List the autenticated(or specified) user's followers
+func (s *UsersService) ListFollowers(user string) ([]*User, error) {
+  var u string
+  if user != "" {
+    u = fmt.Sprintf("users/%v/followers", user)
+  } else {
+    u = "user/followers"
+  }
+  var users []*User
+  req, err := s.client.getReq(u)
+  if err != nil {
+    return nil, err
+  }
+  
+  json.Unmarshal(req, &users)
+
+  return users, nil
+}
+
+// List the users that the autenticated(or specified) user is following
+func (s *UsersService) ListFollowing(user string) ([]*User, error) {
+  var u string
+  if user != "" {
+    u = fmt.Sprintf("users/%v/following", user)
+  } else {
+    u = "user/following"
+  }
+  var users []*User
+  req, err := s.client.getReq(u)
+  if err != nil {
+    return nil, err
+  }
+  
+  json.Unmarshal(req, &users)
+
+  return users, nil
+}
+
