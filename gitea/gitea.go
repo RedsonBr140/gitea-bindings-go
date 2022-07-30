59582f7a88da0ac6fc4f60da1ef8763d9bcbcc1d
kevin <kevinmarqueskmp@gmail.com>
2022-07-30 17:36:42 -0300

    fix: Added the getReq() again to call the newRequest()

--- gitea/gitea.go
+++ gitea/gitea.go
@@ -84,3 +84,7 @@ func (c *Client) newRequest(method string, urlStr string, reqBody []byte) ([]byt
 
 	return responseData, nil
 }
+
+func (c *Client) getReq(urlStr string) ([]byte, error) {
+	return c.newRequest("GET", urlStr, nil)
+}
