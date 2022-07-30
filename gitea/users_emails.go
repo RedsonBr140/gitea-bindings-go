6ef6c0eebd128a9b08914c3a3c67442773638bbe
Redson <redson@riseup.net>
2022-07-29 22:23:30 -0300

    feat: ListEmails

---
+++ gitea/users_emails.go
@@ -0,0 +1,24 @@
+package gitea
+
+import "encoding/json"
+
+type UserEmail struct {
+	Email      string `json:"email,omitempty"`
+	IsVerified bool   `json:"verified,omitempty"`
+	IsPrimary  bool   `json:"primary,omitempty"`
+}
+
+func (s *UsersService) ListEmails() ([]*UserEmail, error) {
+  u := "user/emails"
+  var mailsArray []*UserEmail
+
+  req, err := s.client.getReq(u)
+  if err != nil {
+    return nil, err
+  }
+
+  json.Unmarshal(req, &mailsArray)
+
+  return mailsArray, nil
+}
+
