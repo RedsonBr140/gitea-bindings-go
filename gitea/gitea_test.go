614dcdd118aa1943b348950431d205aeac11b4ce
Redson <redson@riseup.net>
2022-11-29 20:17:09 -0300

    ci: Initial tests.
    Signed-off-by: Redson <redson@riseup.net>

--- gitea/gitea_test.go
+++ gitea/gitea_test.go
@@ -2,9 +2,39 @@ package gitea
 
 import (
 	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"os"
 	"reflect"
 	"testing"
 )
+
+const (
+	baseURLPath = "/api/v1"
+)
+
+func setup() (client *Client, mux *http.ServeMux, serverURL string, teardown func()) {
+	mux = http.NewServeMux()
+
+	apiHandler := http.NewServeMux()
+	apiHandler.Handle(baseURLPath+"/", http.StripPrefix(baseURLPath, mux))
+	apiHandler.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(os.Stderr, "FAIL: Client.BaseURL path prefix is not preserved in the request URL:\n\n")
+		fmt.Fprintf(os.Stderr, "\t%s\n", r.URL.String())
+		fmt.Fprintln(os.Stderr, "\tDid you accidentally use an absolute endpoint URL rather than relative?")
+		http.Error(w, "Client.BaseURL path prefix is not preserved in the request URL.", http.StatusInternalServerError)
+	})
+
+	server := httptest.NewServer(apiHandler)
+
+	client = NewClient(nil)
+	url, _ := url.Parse(server.URL + baseURLPath + "/")
+	client.base = url
+
+	return client, mux, server.URL, server.Close
+}
 
 func testJSONMarshal(t *testing.T, v interface{}, want string) {
 	t.Helper()
@@ -27,5 +57,13 @@ func testJSONMarshal(t *testing.T, v interface{}, want string) {
 
 	if string(w) != string(j) {
 		t.Errorf("json.Marshal(%q) returned %s, want %s", v, j, w)
+	}
+}
+
+func testMethod(t *testing.T, r *http.Request, want string) {
+	t.Helper()
+
+	if got := r.Method; got != want {
+		t.Errorf("Request method: %v, want %v", got, want)
 	}
 }
--- gitea/settings_test.go
+++ gitea/settings_test.go
@@ -1,8 +1,68 @@
 package gitea
 
-import "testing"
+import (
+	"fmt"
+	"net/http"
+	"testing"
 
-func TestAPISettingsMarshal(t *testing.T) {
+	"github.com/google/go-cmp/cmp"
+)
+
+func Test_APISettings(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/settings/api", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		fmt.Fprintf(w, `{"default_git_trees_per_page": 3,"max_response_items":1,"default_paging_num":2,"default_max_blob_size":1998}`)
+	})
+
+	APISettings, err := client.Settings.GetAPISettings()
+	if err != nil {
+		t.Errorf("Settings.GetAPISettings() returned error: %s", err)
+	}
+
+	want := &GeneralAPISettings{
+		DefaultGitTreesPerPage: 3,
+		MaxResponseItems:       1,
+		DefaultPagingNum:       2,
+		DefaultMaxBlobSize:     1998,
+	}
+
+	if !cmp.Equal(APISettings, want) {
+		t.Errorf("Settings.GetAPISettings() returned %v, want %v", APISettings, want)
+	}
+}
+
+func Test_AttachmentSettings(t *testing.T) {
+	client, mux, _, teardown := setup()
+	defer teardown()
+
+	mux.HandleFunc("/settings/attachment", func(w http.ResponseWriter, r *http.Request) {
+		testMethod(t, r, "GET")
+
+		fmt.Fprintf(w, `{"allowed_types":"*/*","enabled":true,"max_files":19,"max_size":2008}`)
+	})
+
+	Attachment, err := client.Settings.GetAttachmentSettings()
+	if err != nil {
+		t.Errorf("Settings.GetAttachmentSettings() returned error: %s", err)
+	}
+
+	want := &GeneralAttachmentSettings{
+		AllowedTypes: "*/*",
+		Enabled:      true,
+		MaxFiles:     int64(19),
+		MaxSize:      int64(2008),
+	}
+
+	if !cmp.Equal(Attachment, want) {
+		t.Errorf("Settings.GetAttachmentSettings() returned %v, want %v", Attachment, want)
+	}
+}
+
+func TestAPISettings_Marshal(t *testing.T) {
 	testJSONMarshal(t, &GeneralAPISettings{}, "{}")
 
 	u := &GeneralAPISettings{
@@ -11,7 +71,6 @@ func TestAPISettingsMarshal(t *testing.T) {
 		DefaultGitTreesPerPage: 3,
 		DefaultMaxBlobSize:     1998,
 	}
-
 	want := `{
 	"max_response_items":1,
 	"default_paging_num":2,
@@ -20,4 +79,65 @@ func TestAPISettingsMarshal(t *testing.T) {
 	}`
 
 	testJSONMarshal(t, u, want)
+}
+
+func TestAttachmentSettings_Marshal(t *testing.T) {
+	testJSONMarshal(t, &GeneralAttachmentSettings{}, "{}")
+
+	u := &GeneralAttachmentSettings{
+		Enabled:      true,
+		AllowedTypes: "*/*",
+		MaxSize:      100,
+		MaxFiles:     20,
+	}
+
+	want := `{
+	"enabled":true,
+	"allowed_types":"*/*",
+	"max_size":100,
+	"max_files":20
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestRepoSettings_Marshal(t *testing.T) {
+	testJSONMarshal(t, &GeneralRepoSettings{}, "{}")
+
+	u := &GeneralRepoSettings{
+		HttpGitDisabled:      true,
+		LfsDisabled:          false,
+		MigrationsDisabled:   true,
+		StarsDisabled:        false,
+		TimeTrackingDisabled: false,
+	}
+
+	want := `{
+	"http_git_disabled":true,
+	"lfs_disabled":false,
+	"migrations_disabled":true,
+	"stars_disabled":false,
+	"time_tracking_disabled":false
+	}`
+
+	testJSONMarshal(t, u, want)
+}
+
+func TestUISettings_Marshal(t *testing.T) {
+	testJSONMarshal(t, &GeneralUISettings{}, "{}")
+
+	u := &GeneralUISettings{
+		AllowedReactions: []string{"+1", "-1", "trollface"},
+		CustomEmojis:     []string{"curitibatm"},
+		DefaultTheme:     "blue",
+	}
+
+	want := `{
+	"default_theme":"blue",
+	"allowed_reactions":["+1","-1","trollface"],
+	"custom_emojis":["curitibatm"]
+	}`
+
+	testJSONMarshal(t, u, want)
+
 }
